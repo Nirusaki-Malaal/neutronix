5e47505239b7691516d4c3b9f6724283443bbf9f
Nirusaki <nirusakimalaal@gmail.com>
2026-06-23 13:30:00 +0530

    feat: expose supervised models in subpackage initialization

---
+++ neutronix/supervised/__init__.py
@@ -0,0 +1,7 @@
+from .gda import GDA
+from .linear_regression import LinearRegression
+from .logistic_regression import LogisticRegression
+from .lwlr import LWLR
+from .naive_bayes import NaiveBayes
+from .perceptron import Perceptron
+from .softmax_regression import SoftmaxRegression
